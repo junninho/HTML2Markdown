4e13c65ebd9ccbad352c223e1bbb9a3850cce9d9
Junninho Thomas <junninhothomas1998@gmail.com>
2017-07-25 17:43:23 -0400

    final changes

--- html2markdown.py
+++ html2markdown.py
@@ -21,30 +21,26 @@ i = 0
 
 
 for post in data['data']['posts']:
-    banners = []
-    title = 'https://www.policygenius.com/blog/'+post['slug']
-    search = '\[banner-.*?]', '\[Banner-.*?]', '\[video-.*?]', '\[recommended-posts .*?]', '\[widget-.*?]', '\[hotspot .*?]'
-    for ban in search:
-        banners += re.findall(ban, post['markdown'])
-    if banners:
-        worksheet.write(row, col,     title)
-        worksheet.write(row, col + 1,     ','.join(banners))
-        row += 1
+	post['markdown'] = h.handle(post['html'])
+	banners = []
+	title = 'https://www.policygenius.com/blog/'+post['slug']
+	search = '\[banner-.*?]', '\[Banner-.*?]', '\[video-.*?]', '\[recommended-posts .*?]', '\[widget-.*?]', '\[hotspot .*?]'
+	for ban in search:
+		banners += re.findall(ban, post['markdown'])
+	if banners:
+		worksheet.write(row, col,     title)
+		worksheet.write(row, col + 1,     ','.join(banners))
+		row += 1
 
+	post['markdown'] = re.sub('\[banner-.*?]', ' ', post['markdown'])
+	post['markdown'] = re.sub('\[Banner-.*?]', ' ', post['markdown'])
+	post['markdown'] = re.sub('\[video-.*?]', ' ', post['markdown'])
+	post['markdown'] = re.sub('\[recommended-posts .*?]', ' ', post['markdown'])
+	post['markdown'] = re.sub('\[widget-.*?]', ' ', post['markdown'])
+	post['markdown'] = re.sub('\[hotspot .*?]', ' ', post['markdown'])
+	post['image'] = re.sub('https://pg-lib', 'https://policygenius-blog', str(post['image']))
 
-    post['markdown'] = h.handle(post['markdown'])
-
-    post['markdown'] = re.sub('\[banner-.*?]', ' ', post['markdown'])
-    post['markdown'] = re.sub('\[Banner-.*?]', ' ', post['markdown'])
-    post['markdown'] = re.sub('\[video-.*?]', ' ', post['markdown'])
-    post['markdown'] = re.sub('\[recommended-posts .*?]', ' ', post['markdown'])
-    post['markdown'] = re.sub('\[widget-.*?]', ' ', post['markdown'])
-    post['markdown'] = re.sub('\[hotspot .*?]', ' ', post['markdown'])
-    post['image'] = re.sub('https://pg-lib', 'https://policygenius-blog', str(post['image']))
-
-
-
-    output_file.write(post['markdown'])
+	output_file.write(post['markdown'])
 object_file.write(json.dumps(data))
 workbook.close()
 
